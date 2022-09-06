2245f573bb582f014b1c7325712e9e16680bae9f
kellydepuy <kellydepuy@gmail.com>
2022-09-06 14:36:42 -0400

    Organized folders, added comments and meta data

--- index.js
+++ index.js
@@ -3,16 +3,23 @@ const apiKey = "&apikey=b934e534"
 let title = ""
 let moviesIdArray = []
 let moviesData = []
-let currentWatchlist = []
 let movieCardsHtml = []
 
+document.getElementById("form").addEventListener("submit", handleSearchClick)
+
+/* This function is called when the user clicks the search button. It stores the searched title in a variable and then fetches
+the results from the imdb api. If no results are returned, the user is informed adn the background icon is removed. Otherwise,
+a for loop goes over the data and pushes each movie's ID to an array. Because the data doesn't contain all the information needed, this function
+calls fetchbyId to get more information. After a timeout, which allows the data collection to complete, createMovieCardHTML is
+called to render the results to the page, and the input is reset for the user's next search*/
 
 function handleSearchClick(e) {
     e.preventDefault()
+    moviesData = []
     title = document.getElementById("search-bar").value
     fetch(`${baseUrl}s=${title}${apiKey}`)
         .then(res => res.json())
-        .then(data => {console.log(data); if (data.Error){
+        .then(data => {if (data.Error){
             document.getElementById("search-results-container").innerHTML = `<p class="missing white center">Sorry! There are no matching movies.</p>`
             document.querySelector("main").classList.remove("main-movie-icon")
         } else {
@@ -22,69 +29,98 @@ function handleSearchClick(e) {
             fetchById()
             setTimeout(createMovieCardHtml, 500)
             document.getElementById("form").reset()
-            createMovieCardHtml()
         }})
     }
 
+/* This function fetches all the data needed to create the movie cards and pushes it to an array. */
+
 function fetchById() {
-    moviesIdArray.map((movie) => {
+    moviesIdArray.map(movie => {
         fetch(`${baseUrl}i=${movie}${apiKey}`)
             .then(res => res.json())
             .then(data => moviesData.push(data))
     })
     }
 
+/* This function removed the background image from the main page, and then it maps over the data to create the html
+for each movie card. It then adds the html to the page so the cards can be displayed, and add and event listener
+to the add to watch list button in each card. It finishes by resetting all the variable in preparation for the
+next search.*/    
+
 function createMovieCardHtml() {
     document.querySelector("main").classList.remove("main-movie-icon")
     movieCardsHtml = moviesData.map((movie) => {
         return (`<div class="card">
-        <img class="movie-poster" src=${movie.Poster} />
-        <div class="text-content">
-            <div class="title-rating">
-                <h2>${movie.Title}</h2>
-                <img class="star" src="./star.png">
-                <p>${movie.imdbRating}</p>
-            </div>
-            <div class="length-genre-watchlist">
+            <img class="movie-poster" src=${movie.Poster} />
+            <div class="text-content">
+                <div class="rating">
+                    <h2>${movie.Title}</h2>
+                    <img class="star" src="./icons/star.png">
+                    <p>${movie.imdbRating}</p>
+                </div>
                 <p>${movie.Runtime}</p>
                 <p>${movie.Genre}</p>
                 <button class="card-btn add-to-watchlist" id=${movie.imdbID}>Add to Watchlist</button>
+                <div class="plot">
+                    <p>${movie.Plot}</p>
+                </div>
             </div>
-            <div class="plot">
-            <p>${movie.Plot}</p>
-            </div>
-        </div>
-        </div>` )}
-    ).join()
+        </div>`)}).join("")
     document.getElementById("search-results-container").innerHTML = movieCardsHtml
     document.querySelectorAll(".add-to-watchlist").forEach(button => button.addEventListener("click", handleAddToWatchlist))
     title = ""
     moviesIdArray = []
-    moviesData = []
-    currentWatchlist = []
     movieCardsHtml = []
 }
 
+/* This function is called when the add to watch list button is pressed. It takes the data required for constructing
+the card and saves it in an object to local storage.*/
+
 function handleAddToWatchlist(event) {
-    const movieId = event.target.id
-    moviesData.map((movie) => {if (movie.imdbID === movieId) {
-        localStorage.setItem(movie.imdbID, JSON.stringify({Title: movie.Title,
-                                                            Poster: movie.Poster,
-                                                            imdbRating: movie.imdbRating, 
-                                                            Runtime: movie.imdbRating,
-                                                            Genre: movie.Genre,
-                                                            imdbID: movie.imdbID,
-                                                            Plot: movie.Plot
-                                                        }))
-    
+    moviesData.map(movie => {
+        if (movie.imdbID === event.target.id) {
+        const movieObj = {
+            Title: movie.Title,
+            Poster: movie.Poster,
+            imdbRating: movie.imdbRating, 
+            Runtime: movie.Runtime,
+            Genre: movie.Genre,
+            imdbID: movie.imdbID,
+            Plot: movie.Plot
+        }
+        localStorage.setItem(movie.imdbID, JSON.stringify(movieObj))
     }})
-    renderWatchlist()
 }
 
 
 
-document.getElementById("form").addEventListener("submit", handleSearchClick)
-
-
+// function createMovieCardHtml() {
+//     document.querySelector("main").classList.remove("main-movie-icon")
+//     movieCardsHtml = moviesData.map((movie) => {
+//         return (`<div class="card">
+//         <img class="movie-poster" src=${movie.Poster} />
+//         <div class="text-content">
+//             <div class="title-rating">
+//                 <h2>${movie.Title}</h2>
+//                 <img class="star" src="./icons/star.png">
+//                 <p>${movie.imdbRating}</p>
+//             </div>
+//             <div class="length-genre-watchlist">
+//                 <p>${movie.Runtime}</p>
+//                 <p>${movie.Genre}</p>
+//                 <button class="card-btn add-to-watchlist" id=${movie.imdbID}>Add to Watchlist</button>
+//             </div>
+//             <div class="plot">
+//             <p>${movie.Plot}</p>
+//             </div>
+//         </div>
+//         </div>` )}
+//     ).join()
+//     document.getElementById("search-results-container").innerHTML = movieCardsHtml
+//     document.querySelectorAll(".add-to-watchlist").forEach(button => button.addEventListener("click", handleAddToWatchlist))
+//     title = ""
+//     moviesIdArray = []
+//     movieCardsHtml = []
+// }
 
 
--- watchlist.js
+++ watchlist.js
@@ -1,3 +1,8 @@
+/* This function retrieves the data needed to create the watch list cards from local storage. It starts by checking
+if local storage has any data. If not, it tells the user to search for movies. If there is data, it creates a
+list of keys adn then pulls the data by its key and creates the html for the card. At the end, it adds the html to the
+page and add event listeners to each movie's remove button.*/
+
 function renderWatchlist() {
     let list = []
     if (localStorage.length < 1) {
@@ -10,25 +15,25 @@ function renderWatchlist() {
             list += `<div class="card">
                         <img class="movie-poster" src=${movie.Poster} />
                         <div class="text-content">
-                            <div class="title-rating">
+                            <div class="rating">
                                 <h2>${movie.Title}</h2>
-                                <img class="star" src="./star.png">
+                                <img class="star" src="./icons/star.png">
                                 <p>${movie.imdbRating}</p>
                             </div>
-                            <div class="length-genre-watchlist">
-                                <p>${movie.Runtime}</p>
-                                <p>${movie.Genre}</p>
-                                <button class="card-btn remove-from-watchlist" id=${movie.imdbID + "remove"}>Remove</button>
-                            </div>
+                            <p class="minutes">${movie.Runtime}</p>
+                            <p class="genre">${movie.Genre}</p>
+                            <button class="card-btn remove-from-watchlist" id=${movie.imdbID + "remove"}>Remove Movie</button>
                             <div class="plot">
-                            <p>${movie.Plot}</p>
+                                <p>${movie.Plot}</p>
                             </div>
                         </div>
-                        </div>` } }
+                    </div>` } }
     
     document.getElementById("watchlist-container").innerHTML = list
     document.querySelectorAll(".remove-from-watchlist").forEach(button => button.addEventListener("click", handleRemoveFromWatchlist))
     }
+
+//This function removes a move from local storage when the remove button is clicked and re-renders the page.
 
 function handleRemoveFromWatchlist(event) {
     localStorage.removeItem(event.target.id.substr(0, 9))
